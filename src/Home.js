bfbb7d661bfd34cf3e3103b48546bc7d5c821e18
Sonny Ornido <sonnyornido0@gmail.com>
2021-09-17 21:56:18 +0800

    Header and Home Page

---
+++ src/Home.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import './Home.css'
+import Product from './Product'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home--container">
+                <img 
+                className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" 
+                alt="" 
+                />
+
+                <div className="home__row">
+                    <Product
+                    title="Lenovo IdeaPad Gaming 3"
+                    price={699.99}
+                    image="https://m.media-amazon.com/images/I/71qblSCB-rL._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={4}
+                    />
+                    <Product
+                    title="Acer Predator Helios 300"
+                    price={1299.99}
+                    image="https://m.media-amazon.com/images/I/719Hcr4vHPS._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={5}
+                    />
+                    <Product
+                    title="Acer Predator Helios 300"
+                    price={888.99}
+                    image="https://m.media-amazon.com/images/I/81cAVFZEj4L._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={5} 
+                    />
+                </div>
+                
+                <div className="home__row">
+                    <Product
+                    title="Alienware m15 R4"
+                    price={3,279.99}
+                    image="https://m.media-amazon.com/images/I/71Hx1SjCFIL._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={5} 
+                    />
+                    <Product
+                    title="ASUS TUF Dash 15"
+                    price={949.99}
+                    image="https://m.media-amazon.com/images/I/81f-JgUXsoS._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={5} 
+                    />
+                    <Product
+                    title="Acer Aspire 5"
+                    price={362.07}
+                    image="https://m.media-amazon.com/images/I/71AmKW4yuMS._AC_UL480_FMwebp_QL65_.jpg"
+                    rating={5} 
+                    />
+                    <Product
+                    title="Lenovo Legion 5"
+                    price={1049.99}
+                    image="https://m.media-amazon.com/images/I/71fzx0pGY5L._AC_UL480_FMwebp_QL65_.jpg"
+                    rating={5} 
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                    title="Anteam 1080p Webcam"
+                    price={25.99}
+                    image="https://m.media-amazon.com/images/I/71Gb-6nihAL._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={4}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhmdh9m9YFTuTkroN6LyWsUJZ-2qZ8zBw",
+  authDomain: "clone-5fb03.firebaseapp.com",
+  projectId: "clone-5fb03",
+  storageBucket: "clone-5fb03.appspot.com",
+  messagingSenderId: "1076536924184",
+  appId: "1:1076536924184:web:a2d3836002c6cd1ff4cb6d",
+  measurementId: "G-KDPX82C8PR"
+};
