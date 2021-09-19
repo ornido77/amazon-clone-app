ce557169cfaf6e3e40baf0aec5156e998b763a75
Sonny Ornido <sonnyornido0@gmail.com>
2021-09-19 16:31:49 +0800

    login page button functions

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    //firebase login stuff here
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    //firebase register stuff here
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -17,12 +32,22 @@ function Login() {
 
         <form action="">
           <h5>Email</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signinBtn">Sign in</button>
+          <button className="login__signinBtn" type="submit" onClick={signIn}>
+            Sign in
+          </button>
         </form>
 
         <p>
@@ -31,7 +56,13 @@ function Login() {
           Interest-Based Ads Notice.
         </p>
 
-        <button className="login__registrationBtn">Create Account</button>
+        <button
+          className="login__registrationBtn"
+          onClick={register}
+          type="submit"
+        >
+          Create Account
+        </button>
       </div>
     </div>
   );
